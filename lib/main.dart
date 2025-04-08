61597247277d5a0a29b5a13d7b20a49118e8cef7
Agure-la <lameckagure@gmail.com>
2025-04-08 16:06:42 +0300

    added signup page

--- lib/main.dart
+++ lib/main.dart
@@ -71,8 +71,42 @@ class LoginCard extends StatelessWidget {
 class SignupCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20.0),
+        child: Padding(
+            padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Full Name'),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(labelText: 'Confirm Password'),
+                obscureText: true,
+              ),
+              SizedBox(height:20),
+              ElevatedButton(
+                onPressed: () {
+                  //handle sign up logic
+                },
+                child: Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-  
 }
