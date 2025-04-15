a712c1c4a9efee327606014853503b1eec629a71
Agure-la <lameckagure@gmail.com>
2025-04-15 11:51:10 +0300

    added assests

--- lib/core/const/path_constants.dart
+++ lib/core/const/path_constants.dart
@@ -1,20 +1,20 @@
 class PathConstants{
   // Onboarding
-  static const String onboarding1 = 'assets/images/onboarding/onboarding.png';
-  static const String onboarding2 = 'assets/images/onboarding/onboarding_2.png';
-  static const String onboarding3 = 'assets/images/onboarding/onboarding_3.png':
+  static const String onboarding1 = 'assets/images/onboarding/onboarding.jpg';
+  static const String onboarding2 = 'assets/images/onboarding/onboarding_2.jpg';
+  static const String onboarding3 = 'assets/images/onboarding/onboarding_3.jpg';
   // Auth
-  static const String eye = 'assets/images/auth/eye_icon.png';
+  static const String eye = 'assets/images/auth/eye_icon.jpg';
   // Tabbar
-  static const String home = 'assets/icons/home/home_icon.png';
-  static const String workouts = 'assets/icons/home/workouts_icon.png';
-  static const String settings = 'assets/icons/home/settings_icon.png';
+  static const String home = 'assets/icons/home/home_icon.jpg';
+  static const String workouts = 'assets/icons/home/workouts_icon.jpg';
+  static const String settings = 'assets/icons/home/settings_icon.jpg';
   // Home
-  static const String profile = 'assets/images/home/profile.png';
-  static const String finished = 'assets/images/home/finished.png';
-  static const String inProgress = 'assets/icons/home/inProgress.png';
-  static const String timeSent = 'assets/icons/home/time.png';
-  static const String cardio = 'assets/images/home/cardio.png';
-  static const String arms = 'assets/images/home/arms.png';
-  static const String progress = 'assets/icons/home/progress.png';
+  static const String profile = 'assets/images/home/profile.jpg';
+  static const String finished = 'assets/images/home/finished.jpg';
+  static const String inProgress = 'assets/icons/home/inProgress.jpg';
+  static const String timeSent = 'assets/icons/home/time.jpg';
+  static const String cardio = 'assets/images/home/cardio.jpg';
+  static const String arms = 'assets/images/home/arms.jpg';
+  static const String progress = 'assets/icons/home/progress.jpg';
 }
