ba5d21890a2bb4ae12f8a5879f6522e0daa02aab
Agure-la <lameckagure@gmail.com>
2025-04-15 10:35:46 +0300

    added data constants

---
+++ lib/core/const/data_constants.dart
@@ -0,0 +1,19 @@
+class DataConstants {
+  static final onboardingTiles = [
+    OnboardingTile(
+      title: TextContstants.onboarding1Title,
+      maintext: TextConstants.onboarding1Description,
+      imagePath: PathConstants.onboarding1
+    ),
+    OnboardingTile(
+        title: TextContstants.onboarding Title,
+        maintext: TextConstants.onboarding Description,
+        imagePath: PathConstants.onboarding2
+    ),
+    OnboardingTile(
+        title: TextContstants.onboarding3Title,
+        maintext: TextConstants.onboarding3Description,
+        imagePath: PathConstants.onboarding3
+    )
+  ];
+}
