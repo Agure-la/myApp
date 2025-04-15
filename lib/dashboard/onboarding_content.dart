09c9e154e9a634f716f150080c5c303ea0a851ca
Agure-la <lameckagure@gmail.com>
2025-04-15 14:07:41 +0300

    added static page

--- lib/dashboard/onboarding_content.dart
+++ lib/dashboard/onboarding_content.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_login/core/const/color_constants.dart';
 import 'package:my_login/core/const/data_constants.dart';
 import 'package:my_login/dashboard/OnboardingBloc.dart';
+import 'package:my_login/dashboard/OnboardingState.dart';
 
 class OnboardingContent extends StatelessWidget {
   @override
@@ -26,11 +28,66 @@ class OnboardingContent extends StatelessWidget {
 
 Widget _createPageView(PageController controller, OnboardingBloc bloc) {
   return PageView(
-      scrollDirection: Axis.horizontal,
-      controller: controller,
-      children: DataConstants.onboardingTiles,
-      onPageChanged: (index) {
-        bloc.add(PageSwipedEvent(index: index));
-      },
-    );
+    scrollDirection: Axis.horizontal,
+    controller: controller,
+    children: DataConstants.onboardingTiles,
+    onPageChanged: (index) {
+      bloc.add(PageSwipedEvent(index: index));
+    },
+  );
+}
+
+Widget _createStatic(OnboardingBloc bloc) {
+  return Column(
+    children: [
+      SizedBox(
+        height: 30,
+      ),
+      BlocBuilder<OnboardingBloc, OnboardingState>(
+        buildWhen: (_, currState) => currState is PageChangedState,
+        builder: (context, state) {
+          return DotsIndicator(
+            dotsCount: 3,
+            position: bloc.pageIndex.toDouble(),
+            decorator: DotsDecorator(
+              Color: Colors.grey,
+              activeColor: ColorConstants.primaryColor,
+            ),
+          );
+        },
+      ),
+      Spacer(),
+      BlocBuilder<OnboardingBloc, OnboardingState>(
+          buildWhen: (_, currState) => currState is PageChangedState,
+          builder: (context, state) {
+            final percent = _getPercent(bloc.pageIndex);
+            return TweenAnimationBuilder<double>(
+              tween: Tween<double>(begin: 0, end: percent),
+              duration: Duration(seconds: 1),
+              builder: (context, value, _) => CircularPercentIndicator(
+                radius: 110,
+                backgroundColor: ColorConstants.primaryColor,
+                progressColor: Colors.white,
+                percent: 1 - value,
+                center: Material(
+                  shape: CircleBorder(),
+                  onPressed: () {
+                    bloc.add(PageChangedEvent());
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Icon(
+                      Icons.east_rounded,
+                      size: 38.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+        SizedBox(height: 30),
+    ],
+  );
 }
